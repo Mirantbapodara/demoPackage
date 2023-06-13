88237ada63685d7c1a2763117e54f60814f4e483
Mirantbapodara <mirantbapodara909@gmail.com>
2023-06-13 22:08:26 +0530

    Add files via upload

---
+++ demo_package2.dart
@@ -0,0 +1,13 @@
+library demo_package2;
+
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+        return Container(color: Colors.red,width: 100,height: 100,);
+  }
+}
